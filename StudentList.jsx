291e2df38cf1e2c4668bb1eae52cf3b0ac9d74e4
KosarAnsari <kosaransari55@gmail.com>
2025-08-11 11:32:21 +0530

    Create StudentList.jsx

---
+++ StudentList.jsx
@@ -0,0 +1,12 @@
+function StudentList({ students }) {
+  return (
+    <>
+      <ul>
+        {students.map((student, index) => (
+          <li key={index}>{student}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+export default StudentList;
